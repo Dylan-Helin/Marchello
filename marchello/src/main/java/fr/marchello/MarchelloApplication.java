4967adbaaf9b80a3ce0db217da5462a844c12ce5
Dylan-Helin <Dylan-Helin@users.noreply.github.com>
2021-10-15 11:56:18 +0200

    Creating Spring boot back
    adding spring boot base files for the project

---
+++ marchello/src/main/java/fr/marchello/MarchelloApplication.java
@@ -0,0 +1,13 @@
+package fr.marchello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarchelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarchelloApplication.class, args);
+	}
+
+}
---
+++ marchello/src/test/java/fr/marchello/MarchelloApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.marchello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarchelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
